005e54e6c9f42b526a6b7cd1d9259304cfd0ae82
leonsanbo <kory.zhou@gmail.com>
2021-11-13 21:13:22 -0800

    update

--- mpi_2dmesh.cpp
+++ mpi_2dmesh.cpp
@@ -448,7 +448,7 @@ sobel_filtered_pixel(float *s, int i, int j , int ncols, int nrows, float *gx, f
    float tmp_x=0.0;
    float tmp_y=0.0;
    //j: row i:col
-   int s_offset = (i-1) + (j-1)*ncols;  
+   int s_offset = (i-1)*ncols + (j-1);  
    
    for (int jj = 0; jj<3; jj++, s_offset += ncols){
       for (int ii = 0; ii<3; ii++){
