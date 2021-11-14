3bca81f010756564eb041dff9fbcf7a54e7eb6b4
leonsanbo <kory.zhou@gmail.com>
2021-11-13 20:47:33 -0800

    update

--- mpi_2dmesh.cpp
+++ mpi_2dmesh.cpp
@@ -430,10 +430,8 @@ recvStridedBuffer(float *dstBuf,
    MPI_Datatype recv_subarray;  // create the mysubarray 
    MPI_Type_create_subarray(2, baseDims, subDims, subOffset, MPI_ORDER_C, MPI_FLOAT, &recv_subarray);
    MPI_Type_commit(&recv_subarray);
-   
-   int dstOffset = dstOffsetRow * dstWidth + dstOffsetColumn;
 
-   MPI_Recv(dstBuf + dstOffset, 1, recv_subarray, fromRank, msgTag, MPI_COMM_WORLD, &status);
+   MPI_Recv(dstBuf, 1, recv_subarray, fromRank, msgTag, MPI_COMM_WORLD, &status);
 
    MPI_Get_count(&status, recv_subarray, &rcount); // check how many MPI_FLOATs we recv'd
 }
